a513f082c21db979323554e5d64a8365aee3c354
lthoedt <lthoedt@gmail.com>
2021-03-25 23:48:21 +0100

    added comment

--- Spotitube/src/main/java/dao/IUserDAO.java
+++ Spotitube/src/main/java/dao/IUserDAO.java
@@ -4,6 +4,8 @@ import javax.sql.DataSource;
 
 import domain.User;
 
+
+// eventueel zou deze class een abstract class kunnen worden om generateToken() een body te geven.
 public interface IUserDAO {
     User loginUser(String username, String password);
     User createUser(String username, String password);
